8bee3a79a6e2b0f3c7f6f38bda3cf57b4d3a66eb
Mikhaelmurmur <stolovich@yahoo.com>
2016-01-06 18:19:21 +0200

    Second Task is done

---
+++ Task2/Task2.cpp
@@ -0,0 +1,28 @@
+// Task2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <string>
+
+int main()
+{
+	char ch = 'a';
+	std::string s = "Hello World!\n";
+	int count;
+	count = 1;
+	const double pi = 3.14;
+	extern int error_number;
+	const char* name = "Njal";
+	const char* season[] = { "1" };
+	struct Date;
+	int day(Date*);
+	double sqrt(double);
+	typedef int PRR;
+	struct User
+	{
+		std::string name;
+	};
+	enum Beer { ca, be };
+	return 0;
+}
+
---
+++ TypesAndDeclarations/TypesAndDeclarations.cpp
@@ -0,0 +1,13 @@
+// TypesAndDeclarations.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!\n";
+    return 0;
+}
+
